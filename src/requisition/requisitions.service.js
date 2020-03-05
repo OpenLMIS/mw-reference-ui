d5eb6a0c9a649f8ef9110782cc114bc23f133c2b
Klaudia Pałkowska <kpalkowska@soldevelo.com>
2020-03-05 13:23:11 +0100

    Updated requisitions service with the latest (requisition-ui 7.0.1) changes

--- src/requisition/requisitions.service.js
+++ src/requisition/requisitions.service.js
@@ -470,7 +470,8 @@
         }
 
         function extendLineItemsWithOrderablesAndFtaps(requisition, statusMessages) {
-            return $q.all([getByVersionIdentities(requisition.availableProducts, new OrderableResource()),
+            var identities = getResourcesFromLineItems(requisition, true);
+            return $q.all([getByVersionIdentities(identities, new OrderableResource()),
                 periodService.get(requisition.processingPeriod.id),
                 // MW-949: Added entire facility object to requisition
                 facilityService.get(requisition.facility.id),
@@ -479,10 +480,13 @@
                 programService.get(requisition.program.id)])
                 // MALAWISUP-1188: ends here
                 .then(function(result) {
-                    requisition.availableFullSupplyProducts =
-                        filterOrderables(true, result[0], requisition.program.id);
-                    requisition.availableNonFullSupplyProducts =
-                        filterOrderables(false, result[0], requisition.program.id);
+                    requisition.requisitionLineItems.forEach(function(lineItem) {
+                        result[0].forEach(function(orderable) {
+                            if (lineItem.orderable.id === orderable.id) {
+                                lineItem.orderable = orderable;
+                            }
+                        });
+                    });
 
                     requisition.processingPeriod = result[1];
                     // MW-949: Added entire facility object to requisition
@@ -494,18 +498,19 @@
                     return requisition;
                 })
                 .then(function(requisition) {
-                    var indentities = getResourcesFromLineItems(requisition.requisitionLineItems);
+                    var identities = getResourcesFromLineItems(requisition, false);
                     return $q.all([
-                        getByVersionIdentities(indentities.orderables, new OrderableResource()),
-                        getByVersionIdentities(indentities.ftaps, new FacilityTypeApprovedProductResource())
+                        getByVersionIdentities(filterOutOrderablesFromLineItems(requisition),
+                            new OrderableResource()),
+                        getByVersionIdentities(identities, new FacilityTypeApprovedProductResource())
                     ])
                         .then(function(result) {
+                            requisition.availableFullSupplyProducts =
+                                filterOrderables(true, result[0], requisition.program.id);
+                            requisition.availableNonFullSupplyProducts =
+                                filterOrderables(false, result[0], requisition.program.id);
+
                             requisition.requisitionLineItems.forEach(function(lineItem) {
-                                result[0].forEach(function(orderable) {
-                                    if (lineItem.orderable.id === orderable.id) {
-                                        lineItem.orderable = orderable;
-                                    }
-                                });
                                 result[1].forEach(function(ftap) {
                                     if (lineItem.approvedProduct && lineItem.approvedProduct.id === ftap.id) {
                                         lineItem.approvedProduct = ftap;
@@ -517,20 +522,19 @@
                 });
         }
 
-        function getResourcesFromLineItems(lineItems) {
-            var orderableIdentities = [],
-                ftapIdentities = [];
-            lineItems.forEach(function(item) {
-                orderableIdentities.push(item.orderable);
-                if (item.approvedProduct) {
-                    ftapIdentities.push(item.approvedProduct);
+        function getResourcesFromLineItems(requisition, isOrderable) {
+            var identities = [];
+            requisition.requisitionLineItems.forEach(function(item) {
+                if (isOrderable) {
+                    identities.push(item.orderable);
+                } else {
+                    var program = getProgramById(item.orderable.programs, requisition.program.id);
+                    if (item.approvedProduct && program.fullSupply && !requisition.emergency) {
+                        identities.push(item.approvedProduct);
+                    }
                 }
             });
-
-            return {
-                ftaps: ftapIdentities,
-                orderables: orderableIdentities
-            };
+            return identities;
         }
 
         function getByVersionIdentities(identities, resource) {
@@ -541,6 +545,19 @@
             return availableProducts.filter(function(product) {
                 var requisitionProgram = getProgramById(product.programs, programId);
                 return requisitionProgram && requisitionProgram.fullSupply === fullSupply;
+            });
+        }
+
+        function filterOutOrderablesFromLineItems(requisition) {
+            return requisition.availableProducts.filter(function(availableProduct) {
+                var shouldBeFetched = true;
+                requisition.requisitionLineItems.forEach(function(lineItem) {
+                    if (lineItem.orderable.id === availableProduct.id
+                        && lineItem.orderable.meta.versionNumber === availableProduct.versionNumber) {
+                        shouldBeFetched = false;
+                    }
+                });
+                return shouldBeFetched;
             });
         }
 
