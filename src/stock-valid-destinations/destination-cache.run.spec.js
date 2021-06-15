5012ce580ecc2107170f591f138c72fd4cf57fb6
saleksandra <soltys.ola@gmail.com>
2021-06-15 10:43:32 +0200

    MALAWISUP-2837: Temporarily disabled cache valid sources and valid destinations on login

---
+++ src/stock-valid-destinations/destination-cache.run.spec.js
@@ -0,0 +1,61 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+describe('destination-cache run', function() {
+
+    beforeEach(function() {
+        var context = this;
+        module('stock-valid-destinations');
+        module('referencedata-user', function($provide) {
+            context.loginServiceSpy = jasmine.createSpyObj('loginService', [
+                'registerPostLoginAction', 'registerPostLogoutAction'
+            ]);
+            $provide.value('loginService', context.loginServiceSpy);
+        });
+
+        inject(function($injector) {
+            this.$rootScope = $injector.get('$rootScope');
+            this.$q = $injector.get('$q');
+            this.sourceDestinationService = $injector.get('sourceDestinationService');
+        });
+
+        this.postLoginAction = getLastCall(this.loginServiceSpy.registerPostLoginAction).args[0];
+
+        spyOn(this.sourceDestinationService, 'clearDestinationsCache');
+    });
+
+    describe('run block', function() {
+
+        it('should register post login action', function() {
+            expect(this.loginServiceSpy.registerPostLoginAction).toHaveBeenCalled();
+        });
+
+    });
+
+    describe('post login action', function() {
+
+        it('should clear valid destinations cache', function() {
+            this.postLoginAction(this.user);
+            this.$rootScope.$apply();
+
+            expect(this.sourceDestinationService.clearDestinationsCache).toHaveBeenCalled();
+        });
+    });
+
+    function getLastCall(method) {
+        return method.calls[method.calls.length - 1];
+    }
+
+});
